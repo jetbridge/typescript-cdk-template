7c7b9198306273b339bda0190203b80376cf11f2
Dizzzmas <yankovskyi.dmytro736@gmail.com>
2020-12-07 21:44:13 +0200

    WIP bash script for initializing project

--- scripts/initializeProject.sh
+++ scripts/initializeProject.sh
@@ -10,22 +10,18 @@ name=${name:-$DEFAULT_NAME}
 if [[ -z "$name" ]]; then exit 1; fi
 
 function rename {
-    subst="perl -i -pe s/TEMPLATE/$name/g"
+    subst="perl -i -pe s/jkv2/$name/g"
 
     # replace TEMPLATE string in files with $name
     find TEMPLATE -type f -exec $subst {} \;
-    $subst serverless.yml package.json app.py tox.ini README.md .github/workflows/pythonapp.yml doc/conf.py doc/Makefile Makefile mypy.ini pyproject.toml
+    $subst package.json
 
     # rename files
-    mv TEMPLATE $name
     mv api.paw ${name}.paw
 }
 
 function install_deps {
-    echo "Installing python dependencies"
-    poetry install
-
-    echo "Installing npm dependencies (for serverless)"
+    echo "Installing npm dependencies"
     npm install
 }
 
@@ -33,12 +29,6 @@ function init_db {
     echo "Creating postgres DB"
     # create pg db
     createdb -e $name || return 0
-
-    echo "Running migrations"
-    poetry run flask db upgrade
-
-    echo "Seeding DB with test data"
-    poetry run flask seed
 }
 
 function init_git {
