47d5485174db885b470b99ace92cc261917ccba6
Mischa Spiegelmock <me@mish.dev>
2021-06-06 23:17:27 +0300

    remove JB account

--- packages/infra/bin/cdk.ts
+++ packages/infra/bin/cdk.ts
@@ -16,6 +16,6 @@ export const prodStack = new InfraStack(app, appName, {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: "736338821564", region: "us-east-1" },
+  // env: { account: "", region: "us-east-1" },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 })
